e3bc6f0691cba4590d3bd4a6dd1b44edef379511
OtisL <42128680+OtisL@users.noreply.github.com>
2020-03-30 16:54:19 -0700

    File Recorder

--- src/_01_File_Recorder/FileRecorder.java
+++ src/_01_File_Recorder/FileRecorder.java
@@ -1,5 +1,20 @@
 package _01_File_Recorder;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
+import javax.swing.JOptionPane;
+
 public class FileRecorder {
 	// Create a program that takes a message from the user and saves it to a file.
+	public static void main(String[] args) {
+		String thing = JOptionPane.showInputDialog("Please put some text here");
+		try {
+			FileWriter fw = new FileWriter("src/_01_File_Recorder/testing.txt");
+			fw.write("\n" + thing);
+			fw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
